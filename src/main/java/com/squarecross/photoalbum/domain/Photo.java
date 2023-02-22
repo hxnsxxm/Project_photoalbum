7e85c9567d9fe34465e157020a09a9694f2390e1
hxnsxxm <108333175+hxnsxxm@users.noreply.github.com>
2023-02-22 19:41:34 +0900

    add Photo Domain

---
+++ src/main/java/com/squarecross/photoalbum/domain/Photo.java
@@ -0,0 +1,35 @@
+package com.squarecross.photoalbum.domain;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "photo", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id", unique = true, nullable = false)
+    private Long photoId;
+
+    @Column(name = "file_name", unique = true)
+    private String fileName;
+
+    @Column(name = "thumb_url", unique = true)
+    private String thumbUrl;
+
+    @Column(name = "original_url", unique = true)
+    private String originalUrl;
+
+    @Column(name = "file_size")
+    private double fileSize;
+
+    @Column(name = "uploaded_at")
+    @CreationTimestamp
+    private Date uploadedAt;
+
+    @Column(name = "album_id")
+    private Long albumId;
+}
