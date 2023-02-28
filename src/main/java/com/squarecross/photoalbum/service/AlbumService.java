bf107d465aae5e6c5403c8097f71493e8b20ca29
hxnsxxm <108333175+hxnsxxm@users.noreply.github.com>
2023-02-28 19:10:58 +0900

    add getAlbumList method

--- src/main/java/com/squarecross/photoalbum/service/AlbumService.java
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -1,6 +1,7 @@
 package com.squarecross.photoalbum.service;
 
 import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.mapper.AlbumMapper;
 import com.squarecross.photoalbum.repository.AlbumRepository;
@@ -12,7 +13,10 @@ import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AlbumService {
@@ -64,5 +68,26 @@ public class AlbumService {
     private void deleteAlbumDirectories(AlbumDto album) throws IOException {
         Files.delete(Paths.get(Constants.PATH_PREFIX + "/photos/original/" + album.getAlbumId()));
         Files.delete(Paths.get(Constants.PATH_PREFIX + "/photos/thumb/" + album.getAlbumId()));
+    }
+
+    public List<AlbumDto> getAlbumList(String keyword, String sort) {
+        List<Album> albums;
+        if (Objects.equals(sort, "byName")) {
+            albums = albumRepository.findByAlbumNameContainingOrderByAlbumNameAsc(keyword);
+        } else if (Objects.equals(sort, "byDate")) {
+            albums = albumRepository.findByAlbumNameContainingOrderByCreatedAtDesc(keyword);
+        } else {
+            throw new IllegalArgumentException("알 수 없는 정렬 기준입니다.");
+        }
+        List<AlbumDto> albumDtos = AlbumMapper.convertToDtoList(albums);
+
+        for (AlbumDto albumDto : albumDtos) {
+            List<Photo> top4 = photoRepository.findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(albumDto.getAlbumId());
+            albumDto.setThumbUrls(top4.stream()
+                                      .map(Photo::getThumbUrl)
+                                      .map(c -> Constants.PATH_PREFIX + c)
+                                      .collect(Collectors.toList()));
+        }
+        return albumDtos;
     }
 }
